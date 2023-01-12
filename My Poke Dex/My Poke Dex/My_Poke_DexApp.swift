f9880a398cca16b64b450ef7f955e015a9cc4990
Dario Santos <dario.santos@hotmail.com>
2023-01-12 17:10:53 +0000

    initial commit

---
+++ My Poke Dex/My Poke Dex/My_Poke_DexApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Poke_DexApp.swift
+//  My Poke Dex
+//
+//  Created by Santos, Dario Ferreira on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct My_Poke_DexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
